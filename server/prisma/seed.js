c7ac8098dba36332e845ca685cf3155f5d1b4f2b
Farshad <f.bagdeli13@gmail.com>
2024-08-14 12:17:36 +0200

    user seed file created

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,25 +1,43 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from 'bcrypt'
-const prisma = PrismaClient();
+import bcrypt from "bcrypt";
+const prisma = new PrismaClient();
 
 async function seed() {
-  const admin = await createUser();
+  const admin = await createUser(
+    "Farshad",
+    "Bagdeli",
+    "0123456789",
+    "Test!234",
+    'F.Bagdeli13@gmail.com'
+  );
+
+  process.exit(0)
 }
 
 async function createUser(
-  name,
-  phone_number,
+  firstName,
+  lastName,
+  phoneNumber,
   password,
-  role = "ADMIN",
-  email
+  email,
+  role = 'ADMIN'
 ) {
   const user = await prisma.user.create({
     data: {
-      name,
-      phone_number,
+      firstName,
+      lastName,
+      phoneNumber,
       password: await bcrypt.hash(password, 10),
+      email,
       role,
-      email: email
-    }
-  })
+    },
+  });
+  console.log('user Created: ', user)
+  return user
 }
+
+seed().catch(async (e) => {
+  console.log(e)
+  await prisma.$disconnect()
+  process.exit(1)
+})
