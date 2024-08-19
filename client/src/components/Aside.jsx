cfe9ed2de4781d622906e102bfea3c43bb464586
Farshad <f.bagdeli13@gmail.com>
2024-08-19 15:03:43 +0200

    aside lists hover done

--- client/src/components/Aside.jsx
+++ client/src/components/Aside.jsx
@@ -1,3 +1,23 @@
 export const Aside = () => {
-  return <aside className="aside"></aside>;
+  return (
+    <aside className="aside">
+      <ul className="aside-ul">
+        <li>
+          <p>Products</p>
+        </li>
+        <li>
+          <p>Customers</p>
+        </li>
+        <li>
+          <p>Suppliers</p>
+        </li>
+        <li>
+          <p>Invoices</p>
+        </li>
+        <li>
+          <p>New Invoice</p>
+        </li>
+      </ul>
+    </aside>
+  );
 };
